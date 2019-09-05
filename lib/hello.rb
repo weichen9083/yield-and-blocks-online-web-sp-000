c4265cd427f85020ab005616793c3373ff44de7b
wei chen <weichen9083@gmail.com>
2019-09-05 20:53:08 +0000

    Automatically backed up by Learn

--- lib/hello.rb
+++ lib/hello.rb
@@ -4,13 +4,8 @@ while i<array.length
 yield array[i]
 i +=1 
 end 
-array
+
 end
 
 # call your method here!
 
-hello_t(["Tim", "Tom", "Jim"]) do |name|
-  if name.start_with?("T")
-    puts "Hi, #{name}"
-  end
-end
